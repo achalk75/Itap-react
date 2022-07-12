08efdc87bf570d717bd136ac6ab2b7336c530a12
Achal Chaudhary <71545457+achalk75@users.noreply.github.com>
2022-07-12 15:06:22 +0530

    Uploading

--- src/DataCompo.js
+++ src/DataCompo.js
@@ -3,7 +3,6 @@ import Item from "./Item";
 
 const DataCompo = ({ data }) => {
   const [query, setQuery] = useState("");
-//   
 
   const handleChange = e => {
       setQuery(e.target.value);
@@ -13,31 +12,34 @@ const DataCompo = ({ data }) => {
   return (
     <>
       <div className="datacompo">
-        <h1>Users List</h1>
+        <h1 style={{textAlign:"center", marginBottom:"20px"}}>Users List</h1>
         <input type="text" id="queryInput" onChange={(e)=>{handleChange(e)}} />
 
         <div className="dataList">
           {
-           data.filter(
-            (person) => {
-              return (
-                person
-                .name
-                .toLowerCase()
-                .includes(query.toLowerCase()) ||
-                person
-                .email
-                .toLowerCase()
-                .includes(query.toLowerCase())
-              );
-            }
-          ).map((e,i)=>{
-            return(
-                <>
-                <Item key={i*3} name={e.name} username={e.username} email={e.email} serial={i+1} phone={e.phone} web={e.website} />
-                </>
-            )
-          })
+
+React.Children.toArray(data.filter(
+    (person) => {
+      return (
+        person
+        .name
+        .toLowerCase()
+        .includes(query.toLowerCase()) ||
+        person
+        .email
+        .toLowerCase()
+        .includes(query.toLowerCase())
+      );
+    }
+  ).map((e,i)=>{
+    return(
+        <>
+        <Item name={e.name} username={e.username} email={e.email} serial={i+1} phone={e.phone} web={e.website} />
+        </>
+    )
+  }))
+
+           
           }
         </div>
       </div>
